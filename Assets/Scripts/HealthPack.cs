ce752a69432f210f101a68d53b2991bca2dab378
merkelmauer <markopetkovic1999@gmail.com>
2022-02-18 16:11:08 +0100

    added stuff to level 1

--- Assets/Scripts/HealthPack.cs
+++ Assets/Scripts/HealthPack.cs
@@ -4,13 +4,15 @@ using UnityEngine;
 
 public class HealthPack : MonoBehaviour
 {
+
+    public int healthAdd = 100;
    
         private void OnTriggerEnter2D(Collider2D other)
         {
             if (other.tag == "Player")
             {
                 var player = other.gameObject;
-                player.GetComponent<HealthManager>().Health += 50;
+                player.GetComponent<HealthManager>().Health += healthAdd;
                 Destroy(gameObject);
             }
         }
